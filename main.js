a10b6a9a270ad324aca489b6ffd40221a2d5e42e
Sona <fastzeusbolt@gmail.com>
2022-07-13 05:40:07 +0300

    script RPS done

--- main.js
+++ main.js
@@ -6,9 +6,39 @@ function computerPlay(){
 
 function playerSelection(){
     Selection=window.prompt("Choose: Rock, Paper, Scissors");
-    return Selection;
+    return Selection
+}
+function playRound(){
+
+let player = playerSelection()
+let computer = computerPlay()
+
+if(player==="Paper" && computer === "Rock"){
+    console.log("You won, Paper beats rock");
+}
+else if (player==="Paper" && computer === "Scissors")
+    console.log("You lost, scissors beat paper");
+
+else if (player==="Rock" && computer ==="Scissors"){
+    console.log("You won, rock beats scissors")
+}
+else if (player==="Rock" && computer === "Paper"){
+    console.log("You lost, paper beats rock");
 }
 
+else if (player==="Scissors" && computer==="Paper"){
+    console.log("You won, Scissors beats peper");
+}
+else if (player==="Scissors" && computer === "Rock"){
+    console.log("You lost, Rock beats scissors");
+}
 
+else console.log('Draw')
+return;
+}
 
-
+function game(){
+for (let i = 0; i < 5; i++) {
+       playRound(); 
+     }
+}
