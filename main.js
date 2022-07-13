d47e6e29614290c270b18d7b56b7b22330e3604f
Sona <fastzeusbolt@gmail.com>
2022-07-13 11:12:07 +0300

    Added Score system

--- main.js
+++ main.js
@@ -13,32 +13,52 @@ function playRound(){
 let player = playerSelection()
 let computer = computerPlay()
 
+
 if(player==="Paper" && computer === "Rock"){
     console.log("You won, Paper beats rock");
+    playerScore++
 }
-else if (player==="Paper" && computer === "Scissors")
+else if (player==="Paper" && computer === "Scissors"){
     console.log("You lost, scissors beat paper");
-
+    computerScore++
+}
 else if (player==="Rock" && computer ==="Scissors"){
     console.log("You won, rock beats scissors")
+    playerScore++
 }
 else if (player==="Rock" && computer === "Paper"){
     console.log("You lost, paper beats rock");
+    computerScore++
 }
 
 else if (player==="Scissors" && computer==="Paper"){
     console.log("You won, Scissors beats peper");
+    playerScore++
 }
 else if (player==="Scissors" && computer === "Rock"){
     console.log("You lost, Rock beats scissors");
+    computerScore++
 }
-
 else console.log('Draw')
-return;
 }
 
 function game(){
 for (let i = 0; i < 5; i++) {
        playRound(); 
+       console.log("Player Score is  " + playerScore  ,"/ Computer Score is " + computerScore);
+     }
+     if (playerScore > computerScore){
+        console.log("You won, the game")
+     }
+     else if (computerScore > playerScore){
+        console.log("You lost, the game")
+     }
+     else if (computerScore===playerScore){
+        console.log("The game is draw")
      }
 }
+
+let playerScore = 0
+let computerScore = 0
+
+game();
